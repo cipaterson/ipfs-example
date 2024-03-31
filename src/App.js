f6bbf34b2228d7cb16f1468747e710c005165c90
Chris Paterson <cipaterson@gmail.com>
2024-03-31 14:19:17 +0800

    small changes to App

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import { useState } from 'react'
 import { create } from 'ipfs-http-client'
 import {Buffer} from 'buffer'
 
-// dotenv.config();
-// console.log(process.env.NODE_ENV)
-const projectId = 'project id';
-const projectSecret = 'project secret';
+const projectId = 'api key here';
+const projectSecret = 'api key secret here';
+const dedicatedGateway = 'sub-domain here'
+
 const auth = 'Basic ' + Buffer.from(projectId + ':' + projectSecret).toString('base64');
 const client = create({
   host: 'ipfs.infura.io',
@@ -16,10 +16,8 @@ const client = create({
   //protocol: 'http',
   apiPath: '/api/v0',
   headers: {
-    'Sec-Fetch-Mode': 'no-cors',
     authorization: auth,
-    Origin: 'http://127.0.0.1:3000',
-    'User-Agent': 'Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19'
+    // 'User-Agent': 'Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile Safari/535.19'
   }
 })
 function App() {
@@ -28,11 +26,11 @@ function App() {
     const file = e.target.files[0]
     try {
       const added = await client.add(file)
-      const url = `https://dedicated_gateway.infura-ipfs.io/ipfs/${added.path}`
+      const url = `https://${dedicatedGateway}.infura-ipfs.io/ipfs/${added.path}`
       updateFileUrl(url)
     } catch (error) {
       console.log('Error uploading file: ', error)
-    }  
+    }
   }
   return (
     <div className="App">
